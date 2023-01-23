1280389ab38ad78f18b5d377d3675aa0875942dc
João Marcos <joaomarcosjmac@gmail.com>
2023-01-23 09:30:18 -0300

    Atualizacao de usuario com PUT

--- src/main/java/com/eujmac/workshopmongo/resources/UserResource.java
+++ src/main/java/com/eujmac/workshopmongo/resources/UserResource.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,6 +26,7 @@ public class UserResource {
 	@Autowired
 	private UserService service;
 	
+	//get todos os usuarios
 	@GetMapping
 	public ResponseEntity<List<UserDTO>> findAll(){
 		List<User> list = service.findAll();
@@ -32,12 +34,14 @@ public class UserResource {
 		return ResponseEntity.ok().body(listdto);
 	}
 	
+	//get usuario por id
 	@GetMapping(value="/{id}")
 	public ResponseEntity<UserDTO> findById(@PathVariable String id){
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(obj));
 	}
 	
+	//insert usuario
 	@PostMapping
 	public ResponseEntity<Void> insert(@RequestBody UserDTO userDTO){
 		User obj = service.fromDTO(userDTO);
@@ -46,9 +50,19 @@ public class UserResource {
 		return ResponseEntity.created(uri).build();
 	}
 	
+	//delete usuario
 	@DeleteMapping(value="/{id}")
 	public ResponseEntity<Void> delete(@PathVariable String id){
 		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	//put usuario
+	@PutMapping(value="/{id}")
+	public ResponseEntity<Void> update(@RequestBody UserDTO userDTO, @PathVariable String id){
+		User obj = service.fromDTO(userDTO);
+		obj.setId(id);
+		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
 	}
 }
